34cf22ae80f1ffad1e6123096f592a95809e3a2e
robert <a1233z51ab10@gmail.com>
2022-03-10 17:50:10 +0800

    add produce_txt_AI_City.py

---
+++ produce_txt_AI_City.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+import pickle
+import os
+from os import listdir, getcwd
+from os.path import join
+
+
+#file = open('image/2022train.txt', 'w')
+
+for i in range(2):
+    if("00001_"+str(i))
+    with open("00001_"+str(i), 'w') as f:
+        f.write("0 0.501953 0.500885 0.996094 0.998230")
